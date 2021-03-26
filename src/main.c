50fa9f9ba9bc7617c542797033ea80161e13ac69
Roman <1815131bill@gmail.com>
2021-03-27 02:13:53 +0300

    Began a write code main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+#include <sqlite3.h>
+
 int main(){
-	printf("Hello world.");
+	char* userName;
+	char* password;
+	printf("Enter your name: ");
+	scanf("%s", &userName);
+	printf("Enter password: ");
+	scanf("%s", &password);
+	printf("Hello %s \n Wellcome to database Music-Salon!", userName);
+
+	do{
+		int choise;
+	
+		printf("Choose Command\n");
+		printf("... 0 - exit");
+		scanf("%d", &choise);
+		switch(choise){
+			case 1:{
+			       
+			       }
+			case 2:{
+			       }
+			default: break;
+		}
+
+	}while(1);
+	
+	printf("Exit...\n");
+	return 1;
 }
